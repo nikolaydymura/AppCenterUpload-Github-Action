595820a9c73f549f9659f96ee36c49114c29d67f
Nikolay Dymura <nikolay.dymura@gmail.com>
2021-09-02 20:46:14 +0300

    Added debug logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 upload_response=$(appcenter distribute release --token ${INPUT_TOKEN} -f ${INPUT_FILE} --output json --group ${INPUT_GROUP:-Collaborators} --app ${INPUT_OWNER}/${INPUT_APP} --release-notes-file ${INPUT_NOTES})
 
-echo "response $upload_response"
-
 release_id=$(echo "$upload_response" | jq -r '.id')
 download_url=$(echo "$upload_response" | jq -r '.downloadUrl')
 install_url=$(echo "$upload_response" | jq -r '.installUrl')
 release_url="https://appcenter.ms/orgs/${INPUT_OWNER}/apps/${INPUT_APP}/distribute/releases/${release_id}"
-
-echo "::debug::install-ur=$install_url"
-echo "::debug::download-url=$download_url"
 
 echo "::set-output name=release-id::$release_id"
 echo "::set-output name=release-url::$release_url"
